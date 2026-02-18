eb6cb0b324a2d706e66f30a753b52e70b7573b80
PC <PC@DESKTOP-OF7RU4P>
2026-02-17 22:45:29 -0700

    BDAProject

--- src/main/java/com/itson/banco_jdbc/Banco_JDBC.java
+++ src/main/java/com/itson/banco_jdbc/Banco_JDBC.java
@@ -12,6 +12,6 @@ public class Banco_JDBC {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        hola mario
+        System.out.println("PINCHI PUTITA");
     }
 }
